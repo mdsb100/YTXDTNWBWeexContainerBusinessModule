214ab4b18bc6a510d0727cdb419e8831009d36a8
lixianke <xianke.li@yintech.cn>
2018-04-16 19:13:40 +0800

    增加Swift文件使编译成功

---
+++ Example/YTXDTNWBWeexContainerBusinessModuleAppExample/makeswiftwork.swift
@@ -0,0 +1,9 @@
+//
+//  makeswiftwork.swift
+//  YTXDTNWBWeexContainerBusinessModuleAppExample
+//
+//  Created by ytx on 2018/4/16.
+//  Copyright © 2018年 CocoaPods. All rights reserved.
+//
+
+import Foundation
